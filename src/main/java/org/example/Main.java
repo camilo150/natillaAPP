d67e3cfd7ef1debb8c3c61c0eca259249bee4da6
GeralMart <geraldine20velasquez@gmail.com>
2023-12-04 15:34:29 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String nombreUsuario = "camilo";
+        Short edad= 19;
+        Long telefono= 3022633615L;
+        String cedula ="1018235200";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctrubre;
+        int cuotaNoviembre;
+        String direccion= "cll 50# 424";
+        double costoInsripcion=150000.0;
+        float tasaDeInteres=0.1F;
+        double saldoFinalUsuario;
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("me llamo pepe cual e u nombre");
+    }
+
+}
